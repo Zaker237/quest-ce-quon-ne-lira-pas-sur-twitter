3a601759b0df98764a95f5c2846b607b6b15ac8a
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-02-21 08:55:43 +0100

    create tabe and insert fonction

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 
 class DateBaseManager(object):
     def __init__(self, db_file="tweets.db"):
@@ -14,11 +14,28 @@ class DateBaseManager(object):
         cursor.close()
 
     def _create_tables(self):
-        pass
-
-    def inset(self):
         cursor = self._open_cursor()
+        table = """create table tweets (
+            id int,
+            tweet_id int,
+            twitter_user_id int,
+            content text,
+            tweet_date text,
+            date text,
+        )"""
+        cursor.execute(table)
+        self._close_cursor(cursor)
 
+    def inset(self, tweet_id, tweet_user_id, content, tweet_date):
+        cursor = self._open_cursor()
+        current_data = datetime.now()
+        req = f"""insert into tweets
+          values ({tweet_id}, {tweet_user_id}, {content}, {tweet_date}, {current_date})
+        """
+        cursor.execute(req)
+
+        # Save (commit) the changes
+        self.conn.commit()
         self._close_cursor(cursor)
 
     def save_db_as_text_file(self, path):
